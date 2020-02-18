d5fcd43639a5430836c774e7794dcb8df8f33d5a
金顺敬 <jim@jimdeMacBook-Pro.local>
2020-02-18 19:10:57 +0800

    Update

---
+++ shiro/src/test/java/org/jim/MyTest.java
@@ -0,0 +1,15 @@
+package org.jim;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class MyTest {
+
+    @Test
+    public void test() {
+        System.out.println("T");
+    }
+
+}
